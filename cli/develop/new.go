ac18a05bc30e6c189d7891eeaea7461fd05b7bad
houzhiliang <bughou@gmail.com>
2016-12-05 18:50:12 +0800

    update fmwk

--- cli/develop/new.go
+++ cli/develop/new.go
@@ -13,21 +13,21 @@ import (
 
 func New(dir string) {
 	proPath := projectPath(dir)
+	checkPkgDir(dir)
 
 	example := filepath.Join(fmwk.Root(), `example`)
-	if !cmd.Ok(cmd.O{}, `cp`, `-r`, example, dir) {
+	if !cmd.Ok(cmd.O{}, `cp`, `-rT`, example, dir) {
 		return
 	}
 
 	appName := filepath.Base(proPath)
 	script := fmt.Sprintf(`
-	cd %s;
-	sed -i'' 's/%s/%s/g' main.go
+	cd %s
 	sed -i'' 's/example/%s/g' .gitignore $(fgrep -rl example release/config)
-	`, dir,
-		strings.Replace(`/`, `\/`, filepath.Join(fmwk.Path(), `example`), -1),
-		strings.Replace(`/`, `\/`, proPath, -1),
-		strings.Replace(`/`, `\/`, appName, -1),
+	sed -i'' 's/%s/%s/g' main.go
+	`, dir, appName,
+		strings.Replace(filepath.Join(fmwk.Path(), `example`), `/`, `\/`, -1),
+		strings.Replace(proPath, `/`, `\/`, -1),
 	)
 	if !cmd.Ok(cmd.O{}, `sh`, `-c`, script) {
 		return
@@ -40,12 +40,12 @@ func checkPkgDir(dir string) {
 	case err == nil:
 		if fi.IsDir() {
 			if !utils.IsEmptyDir(dir) {
-				fmt.Println(dir, `is not empty.`)
-				os.Exit(1)
+				fmt.Println(dir, `exist and is not empty.`)
+				os.Exit(0)
 			}
 		} else {
 			fmt.Println(dir, `is not a dir.`)
-			os.Exit(1)
+			os.Exit(0)
 		}
 	case os.IsNotExist(err):
 		if err := os.MkdirAll(dir, 0775); err != nil {
@@ -59,7 +59,7 @@ func checkPkgDir(dir string) {
 func projectPath(dir string) string {
 	if dir == `` {
 		fmt.Println(`project dir can't be empty.`)
-		os.Exit(1)
+		os.Exit(0)
 	}
 
 	if !filepath.IsAbs(dir) {
@@ -69,20 +69,20 @@ func projectPath(dir string) string {
 		}
 	}
 
-	goenv := os.Getenv(`GOENV`)
-	if goenv == `` {
-		fmt.Println(`no GOENV environment variable set.`)
-		os.Exit(1)
+	gopath := os.Getenv(`GOPATH`)
+	if gopath == `` {
+		fmt.Println(`no GOPATH environment variable set.`)
+		os.Exit(0)
 	}
-	goenv = filepath.Join(goenv, `src`)
+	gopath = filepath.Join(gopath, `src`)
 
-	rel, err := filepath.Rel(goenv, dir)
+	rel, err := filepath.Rel(gopath, dir)
 	if err != nil {
 		panic(err)
 	}
 	if rel[0] == '.' {
-		fmt.Printf("project dir must be under %s.\n", goenv)
-		os.Exit(1)
+		fmt.Printf("project dir must be under %s.\n", gopath)
+		os.Exit(0)
 	}
 	return rel
 }
--- config/fmwk/root.go
+++ config/fmwk/root.go
@@ -2,9 +2,7 @@ package fmwk
 
 import (
 	"os"
-	"path"
 	"path/filepath"
-	"reflect"
 
 	"github.com/bughou-go/xiaomei/utils"
 )
@@ -12,17 +10,19 @@ import (
 var rootDir string
 
 func Path() string {
-	return path.Dir(path.Dir(reflect.TypeOf(struct{}{}).PkgPath()))
+	return `github.com/bughou-go/xiaomei`
 }
 
 func Root() string {
 	if rootDir != `` {
 		return rootDir
 	}
-	fmwk := Path()
-	if vendorPkg := filepath.Join(Root(), `vendor`, fmwk); utils.Exist(vendorPkg) {
-		rootDir = vendorPkg
-	} else if globalPkg := filepath.Join(os.Getenv(`GOPATH`), `src`, fmwk); utils.Exist(globalPkg) {
+	/*
+		if vendorPkg := filepath.Join(Root(), `vendor`, Path()); utils.Exist(vendorPkg) {
+			rootDir = vendorPkg
+		} else
+	*/
+	if globalPkg := filepath.Join(os.Getenv(`GOPATH`), `src`, Path()); utils.Exist(globalPkg) {
 		rootDir = globalPkg
 	} else {
 		panic(`framework not found.`)
