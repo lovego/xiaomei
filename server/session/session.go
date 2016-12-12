c15739da71b1dd80262f1cb183044d17ad0fe9b5
houzhiliang <bughou@gmail.com>
2016-12-12 11:14:19 +0800

    start to add cookieSesson

--- server/session/session.go
+++ server/session/session.go
@@ -1,6 +1,7 @@
 package session
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
@@ -10,5 +11,21 @@ type Session interface {
 }
 
 type CookieSession struct {
-	Name string
+	http.Cookie
+}
+
+func (cs *CookieSession) Get(req *http.Request, p interface{}) {
+	cookie, _ := req.Cookie(cs.Cookie.Name)
+	if cookie != nil && cookie.Value != `` {
+		json.Unmarshal()
+	}
+}
+
+func (cs *CookieSession) Set(req *http.Request, res http.ResponseWriter, data interface{}) {
+}
+
+func (cs *CookieSession) verifySig() {
+}
+
+func (cs *CookieSession) genSig() {
 }
