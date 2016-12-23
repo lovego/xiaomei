790cba3fde32703ac6d1cd9728236e8047150dd7
廖恒晨 <liaohengchen@gmail.com>
2016-12-23 18:47:03 +0800

    fix: generate a random port

--- cli/develop/new.go
+++ cli/develop/new.go
@@ -1,13 +1,15 @@
 package develop
 
 import (
-	"crypto/rand"
+	crand "crypto/rand"
 	"encoding/hex"
 	"errors"
 	"fmt"
+	mrand "math/rand"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/bughou-go/xiaomei/config"
 	"github.com/bughou-go/xiaomei/utils"
@@ -28,18 +30,21 @@ func New(dir string) error {
 		return errors.New(`cp templates failed.`)
 	}
 
+	r := mrand.New(mrand.NewSource(time.Now().UnixNano()))
+	port := r.Intn(10000) + 30000
 	appName := filepath.Base(proPath)
 	script := fmt.Sprintf(`
 	cd %s
 	sed -i'' 's/example/%s/g' .gitignore $(fgrep -rl example release/config)
 	sed -i'' 's/%s/%s/g' main.go
 	sed -i'' 's/secret-string/%s/g' release/config/envs/production.yml
+	sed -i'' 's/3000/%d/g' release/config/config.yml
 	ln -sf envs/dev.yml release/config/env.yml 2>/dev/null ||
 	cp -f release/config/envs/dev.yml release/config/env.yml
 	`, dir, appName,
 		strings.Replace(filepath.Join(config.Fmwk.Path(), `example`), `/`, `\/`, -1),
 		strings.Replace(proPath, `/`, `\/`, -1),
-		generateSecret(),
+		generateSecret(), port,
 	)
 	if !cmd.Ok(cmd.O{}, `sh`, `-c`, script) {
 		return errors.New(`process templates failed.`)
@@ -50,7 +55,7 @@ func New(dir string) error {
 // 32 byte hex string
 func generateSecret() string {
 	b := make([]byte, 16)
-	if _, err := rand.Read(b); err != nil {
+	if _, err := crand.Read(b); err != nil {
 		panic(err)
 	}
 	return hex.EncodeToString(b)
