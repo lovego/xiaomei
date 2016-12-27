e75b323fe2e407e7a16def0937eafdcd8c063fe6
廖恒晨 <liaohengchen@gmail.com>
2016-12-27 18:52:57 +0800

    redo utils number thousand separator

--- utils/number_test.go
+++ utils/number_test.go
@@ -1,59 +1,23 @@
 package utils
 
 import (
-	"gopkg.in/inf.v0"
-	"strconv"
 	"testing"
 )
 
-func TestThousandSep(t *testing.T) {
-	cases := map[string]string{
-		`1`:          `1`,
-		`12`:         `12`,
-		`123`:        `123`,
-		`1234`:       `1,234`,
-		`12345`:      `12,345`,
-		`123456`:     `123,456`,
-		`1234567`:    `1,234,567`,
-		`1234567.8`:  `1,234,567.8`,
-		`1234567.89`: `1,234,567.89`,
+func TestThousand(t *testing.T) {
+	cases := map[string]interface{}{
+		`1`:            `1`,
+		`12`:           12,
+		`123`:          `123`,
+		`1,234`:        `1234`,
+		`12,345`:       `12345`,
+		`123,456`:      `123456`,
+		`1,234,567`:    `1234567`,
+		`1,234,567.8`:  `1234567.8`,
+		`1,234,567.89`: 1234567.89,
 	}
-	for input, expect := range cases {
+	for expect, input := range cases {
 		if got := ThousandSep(input); got != expect {
-			t.Errorf("input: %s, expect: %s, got: %s\n", input, expect, got)
-		}
-		input = `-` + input
-		expect = `-` + expect
-		if got := ThousandSep(input); got != expect {
-			t.Errorf("input: %s, expect: %s, got: %s\n", input, expect, got)
-		}
-	}
-}
-
-func TestThousands(t *testing.T) {
-	cases := map[string]string{
-		`4`:          `0.00`,
-		`12`:         `0.01`,
-		`123`:        `0.12`,
-		`1234`:       `1.23`,
-		`12345`:      `12.35`,
-		`123456`:     `123.46`,
-		`1234567`:    `1,234.57`,
-		`1234567.8`:  `1,234.57`,
-		`1234567.89`: `1,234.57`,
-	}
-	dec := new(inf.Dec)
-	for input, expect := range cases {
-		dec.SetString(input)
-		if got := Thousands(dec); got != expect {
-			t.Errorf("input: %s, expect: %s, got: %s\n", input, expect, got)
-		}
-		if i, _ := strconv.ParseFloat(input, 64); i >= 5 {
-			expect = `-` + expect
-		}
-		input = `-` + input
-		dec.SetString(input)
-		if got := Thousands(dec); got != expect {
 			t.Errorf("input: %s, expect: %s, got: %s\n", input, expect, got)
 		}
 	}
