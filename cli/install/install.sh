dc6929743e6e0ec4740d38c0e4b0395d09afdb07
houzhiliang <bughou@gmail.com>
2017-01-12 10:55:08 +0800

    add install docker

--- cli/install/install.sh
+++ cli/install/install.sh
@@ -7,6 +7,7 @@ main() {
   which redis-server || install redis-server
   install_mysql_server
   install_mongodb_shell
+  install_docker
 }
 
 setup_sudo_no_password() {
@@ -17,20 +18,28 @@ setup_sudo_no_password() {
 
 
 install_mysql_server() {
-  if ! which mysqld; then
-    sudo debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password password root"
-    sudo debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password_again password root"
-    install mysql-server-5.6
-  fi
+  which mysqld && return
+  sudo debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password password root"
+  sudo debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password_again password root"
+  install mysql-server-5.6
 }
 
 install_mongodb_shell() {
-  if ! which mongo; then
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-    echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-    sudo apt-get update
-    sudo apt-get install -y mongodb-org-shell
-  fi
+  which mongo && return
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+  echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+  sudo apt-get update
+  sudo apt-get install -y mongodb-org-shell
+}
+
+install_docker() {
+  which docker && return
+  sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+    --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+  echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
+  sudo apt-get update
+  sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual docker-engine 
 }
 
 install() {
