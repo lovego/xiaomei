b1572c8469b5d616dd556e6e25f5755e5d1d6213
houzhiliang <bughou@gmail.com>
2017-03-17 01:26:59 +0800

    optimize logs

--- xiaomei/stack/logs.go
+++ xiaomei/stack/logs.go
@@ -2,7 +2,6 @@ package stack
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/bughou-go/xiaomei/utils/cmd"
 	"github.com/bughou-go/xiaomei/xiaomei/cluster"
@@ -24,35 +23,26 @@ func Logs(svcName string, all bool) error {
 
 func serviceLog(svcName string, all bool) error {
 	for _, node := range cluster.GetCluster().Nodes() {
-		if has, err := nodeLog(svcName, all, node); err != nil {
-			return err
-		} else if !all && has {
+		if nodeLog(svcName, all, node) && !all {
 			return nil
 		}
 	}
 	return nil
 }
 
-func nodeLog(svcName string, all bool, node cluster.Node) (bool, error) {
-	script := fmt.Sprintf(`docker ps -aqf name=%s_%s.`, release.Name(), svcName)
-	if output, err := node.Run(cmd.O{Output: true}, script); err != nil {
-		return false, err
-	} else {
-		containerIds := strings.Split(output, "\n")
-		return len(containerIds) > 0, containersLog(svcName, all, node, containerIds)
+func nodeLog(svcName string, all bool, node cluster.Node) bool {
+	var head string
+	if !all {
+		head = "| head -n1"
 	}
-}
-
-func containersLog(svcName string, all bool, node cluster.Node, containerIds []string) error {
-	for _, containerId := range containerIds {
-		println(color.GreenString(`%s %s:`, svcName, containerId))
-
-		if _, err := node.Run(cmd.O{}, fmt.Sprintf(`docker logs %s`, containerId)); err != nil {
-			return err
-		}
-		if !all {
-			break
-		}
-	}
-	return nil
+	script := fmt.Sprintf(`
+ids=$(docker ps -aqf name=%s_%s.)
+if test -n "$ids"; then
+	echo "$ids" %s | xargs -rn1 sh -c 'echo %s $0; docker logs $0'
+fi
+	`,
+		release.Name(), svcName, head, color.GreenString(svcName),
+	)
+	_, err := node.Run(cmd.O{}, script)
+	return err == nil
 }
