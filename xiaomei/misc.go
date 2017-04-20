9e7bad4b553094bbe38fcf93d6d02890473f59da
houzhiliang <bughou@gmail.com>
2017-04-20 16:55:20 +0800

    save

--- xiaomei/misc.go
+++ xiaomei/misc.go
@@ -40,7 +40,7 @@ func versionCmd() *cobra.Command {
 		Use:   `version`,
 		Short: `show xiaomei version.`,
 		RunE: release.NoArgCall(func() error {
-			println(`xiaomei version 17.4.13`)
+			println(`xiaomei version 17.4.20`)
 			return nil
 		}),
 	}
