1debdbe1722df24af5d0ffc17e25ddd326deab85
houzhiliang <bughou@gmail.com>
2017-04-24 20:53:41 +0800

    split mergeMap func

--- utils/merge.go
+++ utils/merge.go
@@ -27,33 +27,37 @@ func mergeMap(a, b reflect.Value, keys string) {
 			key = key.Elem()
 		}
 		if bv.Kind() == reflect.Map {
-			switch a.Kind() {
-			case reflect.Map:
-				mergeMap(a.MapIndex(key), bv, keys+`.`+key.String())
-			case reflect.Struct:
-				keyStr := strings.Title(key.String())
-				mergeMap(a.FieldByName(keyStr), bv, keys+`.`+keyStr)
-			default:
-				panic(`a` + keys + ` should be a map or struct`)
-			}
+			mergeMapRecursively(a, key, bv, keys)
 		} else {
-			switch a.Kind() {
-			case reflect.Map:
-				if key.Kind() == reflect.Interface {
-					key = key.Elem()
-				}
-				a.SetMapIndex(key, bv)
-			case reflect.Struct:
-				keyStr := strings.Title(key.String())
-				if av := a.FieldByName(keyStr); av.IsValid() {
-					println(keyStr)
-					av.Set(bv)
-				} else {
-					panic(`no such field: ` + keyStr)
-				}
-			default:
-				panic(`a` + keys + ` should be a map or struct`)
-			}
+			mergeMapValue(a, key, bv, keys)
 		}
+	}
+}
+
+func mergeMapRecursively(a, key, bv reflect.Value, keys string) {
+	switch a.Kind() {
+	case reflect.Map:
+		mergeMap(a.MapIndex(key), bv, keys+`.`+key.String())
+	case reflect.Struct:
+		keyStr := strings.Title(key.String())
+		mergeMap(a.FieldByName(keyStr), bv, keys+`.`+keyStr)
+	default:
+		panic(`a` + keys + ` should be a map or struct`)
+	}
+}
+
+func mergeMapValue(a, key, bv reflect.Value, keys string) {
+	switch a.Kind() {
+	case reflect.Map:
+		a.SetMapIndex(key, bv)
+	case reflect.Struct:
+		keyStr := strings.Title(key.String())
+		if av := a.FieldByName(keyStr); av.IsValid() {
+			av.Set(bv)
+		} else {
+			panic(`no such field: ` + keyStr)
+		}
+	default:
+		panic(`a` + keys + ` should be a map or struct`)
 	}
 }
