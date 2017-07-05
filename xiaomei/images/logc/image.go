24d02317d17ad964d5a42cb73bc81b13a0baf1b5
houzhiliang <bughou@gmail.com>
2017-07-05 17:14:28 +0800

    logc GOENV environment variable

--- xiaomei/images/logc/image.go
+++ xiaomei/images/logc/image.go
@@ -1,4 +1,12 @@
 package logc
 
+import (
+	"github.com/lovego/xiaomei/xiaomei/release"
+)
+
 type Image struct {
+}
+
+func (i Image) Envs() []string {
+	return []string{`GOENV=` + release.Env()}
 }
