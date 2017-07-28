d7568269df3635772f4f0dcf4e3ce4783fbdd599
houzhiliang <bughou@gmail.com>
2017-07-28 10:21:21 +0800

    update slice functions

--- utils/slice/slice.go
+++ utils/slice/slice.go
@@ -18,7 +18,7 @@ func ContainsInt(slice []int, target int) bool {
 	return false
 }
 
-func Index(slice []string, target string) int {
+func IndexString(slice []string, target string) int {
 	for i := 0; i < len(slice); i++ {
 		if slice[i] == target {
 			return i
@@ -27,11 +27,43 @@ func Index(slice []string, target string) int {
 	return -1
 }
 
-func LastIndex(slice []string, target string) int {
+func LastIndexString(slice []string, target string) int {
 	for i := len(slice) - 1; i >= 0; i-- {
 		if slice[i] == target {
 			return i
 		}
 	}
 	return -1
+}
+
+func RemoveString(slice, toRemove []string) (result []string) {
+	if len(slice) == 0 || len(toRemove) == 0 {
+		return slice
+	}
+	m := make(map[string]bool)
+	for _, v := range toRemove {
+		m[v] = true
+	}
+	for _, v := range slice {
+		if !m[v] {
+			result = append(result, v)
+		}
+	}
+	return
+}
+
+func RemoveInt(slice, toRemove []int) (result []int) {
+	if len(slice) == 0 || len(toRemove) == 0 {
+		return slice
+	}
+	m := make(map[int]bool)
+	for _, v := range toRemove {
+		m[v] = true
+	}
+	for _, v := range slice {
+		if !m[v] {
+			result = append(result, v)
+		}
+	}
+	return
 }
