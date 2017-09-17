a3a771c860656a88b26a66ea46739d33784eaf52
houzhiliang <bughou@gmail.com>
2017-09-17 14:43:01 +0800

    routes name fix

--- xiaomei/new/webapp/routes/routes.go
+++ xiaomei/new/webapp/routes/routes.go
@@ -17,44 +17,44 @@ type session struct {
 }
 
 func Routes() *router.Router {
-	router := router.New()
+	routes := router.New()
 
-	router.Get(`/`, func(req *xiaomei.Request, res *xiaomei.Response) {
+	routes.Get(`/`, func(req *xiaomei.Request, res *xiaomei.Response) {
 		res.Json(map[string]string{`hello`: config.DeployName()})
 		req.Log(map[string]interface{}{`hello`: `world`, `i'm`: `xiaomei`})
 	})
 
-	router.Get(`/result`, func(req *xiaomei.Request, res *xiaomei.Response) {
+	routes.Get(`/result`, func(req *xiaomei.Request, res *xiaomei.Response) {
 		res.Result([]string{`hello`, `world`}, nil)
 	})
 
-	router.Get(`/result2`, func(req *xiaomei.Request, res *xiaomei.Response) {
+	routes.Get(`/result2`, func(req *xiaomei.Request, res *xiaomei.Response) {
 		res.Result([]string{`hello`, `world`}, errs.New(`captcha-err`, `验证码错误`))
 	})
 
-	router.Get(`/result-err`, func(req *xiaomei.Request, res *xiaomei.Response) {
+	routes.Get(`/result-err`, func(req *xiaomei.Request, res *xiaomei.Response) {
 		res.Result([]string{`hello`, `world`}, errors.New(`unknown error`))
 	})
 
-	router.Get(`/json-err`, func(req *xiaomei.Request, res *xiaomei.Response) {
+	routes.Get(`/json-err`, func(req *xiaomei.Request, res *xiaomei.Response) {
 		res.Result([]string{`hello`, `world`}, errs.Trace(errors.New(`unknown error`)))
 	})
 
-	router.Get(`/session-get`, func(req *xiaomei.Request, res *xiaomei.Response) {
+	routes.Get(`/session-get`, func(req *xiaomei.Request, res *xiaomei.Response) {
 		var sess = session{}
 		req.Session(&sess)
 		res.Json(sess)
 	})
 
-	router.Get(`/session-set`, func(req *xiaomei.Request, res *xiaomei.Response) {
+	routes.Get(`/session-set`, func(req *xiaomei.Request, res *xiaomei.Response) {
 		var sess = session{UserId: 1, UserName: `xiaomei`, LoginTime: time.Now()}
 		res.Session(sess)
 		res.Json(sess)
 	})
 
-	router.Get(`/session-delete`, func(req *xiaomei.Request, res *xiaomei.Response) {
+	routes.Get(`/session-delete`, func(req *xiaomei.Request, res *xiaomei.Response) {
 		res.Session(nil)
 	})
 
-	return router
+	return routes
 }
