de2ff5900cbdf4faab3885d8d609e23894d60183
houzhiliang <bughou@gmail.com>
2017-10-09 20:03:45 +0800

    map GOROOT into workspace_godoc

--- xiaomei/workspace_godoc/deploy.go
+++ xiaomei/workspace_godoc/deploy.go
@@ -8,12 +8,17 @@ import (
 )
 
 func deploy() error {
+	goRoot := os.Getenv(`GOROOT`)
+	if goRoot == `` {
+		goRoot = `/usr/local/go`
+	}
 	script := fmt.Sprintf(`
   docker stop workspace-godoc >/dev/null 2>&1 && docker rm workspace-godoc
 	docker run --name=workspace-godoc -d --restart=always \
 	--network=host -e=GODOCPORT=1234 \
-	-v %s:/home/ubuntu/go hub.c.163.com/lovego/xiaomei/godoc
-	`, os.Getenv(`GOPATH`))
+	-v %s:/home/ubuntu/go -v %s:/usr/local/go \
+	hub.c.163.com/lovego/xiaomei/godoc
+	`, os.Getenv(`GOPATH`), goRoot)
 	_, err := cmd.Run(cmd.O{}, `sh`, `-c`, script)
 	return err
 }
