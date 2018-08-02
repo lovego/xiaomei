36c23f9f4accaa3afb3dc7ebdfa4497093ae5dde
chenyun <yun.chen@hztl3.com>
2018-08-02 10:39:22 +0800

    添加排出test下依赖包

--- xiaomei/images/app/dependences.go
+++ xiaomei/images/app/dependences.go
@@ -13,23 +13,22 @@ import (
 
 func depsCmd() *cobra.Command {
 	var inVendor bool
+	var exclude bool
 	cmd := &cobra.Command{
 		Use:   `deps`,
 		Short: "list dependence packages.",
 		Run: func(c *cobra.Command, args []string) {
-			deps := getDeps(inVendor)
-			// deps := testDeps()
+			deps := append(getDeps(inVendor), testDeps(exclude)...)
 			fmt.Println(strings.Join(deps, "\n"))
 		},
 	}
 	cmd.Flags().BoolVarP(&inVendor, `in-vendor`, `v`, false, `list dependences in vendor dir.`)
-	cmd.Flags().BoolVarP(&inVendor, `exclude-test`, `e`, false,
+	cmd.Flags().BoolVarP(&exclude, `exclude-test`, `e`, false,
 		`list dependences in vendor dir exclude test.`)
 	return cmd
 }
 
 func getDeps(inVendor bool) (deps []string) {
-	fmt.Println("test =====-----", inVendor)
 	result, err := cmd.Run(
 		cmd.O{Output: true, Dir: path.Join(release.Root(), `../`)},
 		`go`, `list`, `-e`, `-f`, `{{join .Deps "\n"}}`,
@@ -38,29 +37,30 @@ func getDeps(inVendor bool) (deps []string) {
 		log.Panic(err)
 	}
 
+	pkgs := strings.Split(result, "\n")
 	projectPath := release.Path()
 	vendorPath := path.Join(projectPath, `vendor`)
 	if inVendor {
-		pkgs := strings.Split(result, "\n")
 		for _, pkg := range pkgs {
 			if strings.HasPrefix(pkg, vendorPath) {
 				deps = append(deps, pkg)
 			}
 		}
 	} else {
-		pkgs := strings.Split(result, "/n")
 		for _, pkg := range pkgs {
 			if strings.Contains(pkg, `.`) && !strings.HasPrefix(pkg, projectPath) {
 				deps = append(deps, pkg)
 			}
 		}
-
-		deps = append(deps, testDeps()...)
 	}
+
 	return
 }
 
-func testDeps() (deps []string) {
+func testDeps(exclude bool) (deps []string) {
+	if exclude {
+		return
+	}
 	result, err := cmd.Run(
 		cmd.O{Output: true, Dir: path.Join(release.Root(), `../`)},
 		`go`, `list`, `-e`, `-f`, `{{join .TestImports "\n"}}`, `./models/...`,
@@ -69,8 +69,11 @@ func testDeps() (deps []string) {
 		log.Panic(err)
 	}
 	pkgs := strings.Split(result, "\n")
+	projectPath := release.Path()
 	for _, pkg := range pkgs {
-		deps = append(deps, pkg)
+		if strings.Contains(pkg, `.`) && !strings.HasPrefix(pkg, projectPath) {
+			deps = append(deps, pkg)
+		}
 	}
 	return
 }
