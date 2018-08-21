aae201415fde128817f3a9088074841cbaafd03d
bughou <applejava@qq.com>
2018-08-21 16:52:52 +0800

    update redisdb

--- config/db/redisdb/redisdb.go
+++ config/db/redisdb/redisdb.go
@@ -25,13 +25,14 @@ func Pool(name string) *redis.Pool {
 }
 
 func newPool(name string) *redis.Pool {
+	var url = config.Get(`redis`).GetString(name)
 	return &redis.Pool{
 		MaxIdle:     32,
 		MaxActive:   128,
 		IdleTimeout: 600 * time.Second,
 		Dial: func() (redis.Conn, error) {
 			return redis.DialURL(
-				config.Get(`redis`).GetString(name),
+				url,
 				redis.DialConnectTimeout(3*time.Second),
 				redis.DialReadTimeout(3*time.Second),
 				redis.DialWriteTimeout(3*time.Second),
