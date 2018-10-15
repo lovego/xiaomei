65bd58cec202df494c2c9f55a9461541f1f57089
bughou <applejava@qq.com>
2018-10-15 21:18:42 +0800

    db connetion string params

--- config/db/mysql/mysql.go
+++ config/db/mysql/mysql.go
@@ -3,39 +3,97 @@ package mysql
 import (
 	"database/sql"
 	"log"
+	"net/url"
+	"strconv"
 	"sync"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/lovego/bsql"
 	"github.com/lovego/xiaomei/config"
 )
 
-var mysqlDBs = struct {
+var postgresDBs = struct {
 	sync.Mutex
-	m map[string]*sql.DB
-}{m: make(map[string]*sql.DB)}
+	m map[string]*bsql.DB
+}{m: make(map[string]*bsql.DB)}
 
-func DB(name string) *sql.DB {
-	mysqlDBs.Lock()
-	defer mysqlDBs.Unlock()
-	db := mysqlDBs.m[name]
+func DB(name string) *bsql.DB {
+	postgresDBs.Lock()
+	defer postgresDBs.Unlock()
+	db := postgresDBs.m[name]
 	if db == nil {
 		db = newDB(name)
-		mysqlDBs.m[name] = db
+		postgresDBs.m[name] = db
 	}
 	return db
 }
 
-func newDB(name string) *sql.DB {
-	db, err := sql.Open(`mysql`, config.Get(`mysql`).GetString(name))
+func newDB(name string) *bsql.DB {
+	dataSource := newDataSource(config.Get("mysql").GetString(name))
+	db, err := sql.Open("mysql", dataSource.url.String())
 	if err != nil {
 		log.Panic(err)
 	}
 	if err := db.Ping(); err != nil {
 		log.Panic(err)
 	}
-	db.SetConnMaxLifetime(time.Minute * 10)
-	db.SetMaxIdleConns(5)
-	db.SetMaxOpenConns(50)
-	return db
+	db.SetMaxOpenConns(dataSource.maxOpen)
+	db.SetMaxIdleConns(dataSource.maxIdle)
+	db.SetConnMaxLifetime(dataSource.maxLife)
+	return bsql.New(db, 5*time.Second)
+}
+
+type dataSource struct {
+	url              *url.URL
+	maxIdle, maxOpen int
+	maxLife          time.Duration
+}
+
+func newDataSource(urlStr string) dataSource {
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		log.Panic(err)
+	}
+	ds := dataSource{url: u}
+
+	q := u.Query()
+	if str := q.Get("maxIdle"); str != "" {
+		q.Del("maxIdle")
+		ds.maxIdle = parseInt(str)
+	} else {
+		ds.maxIdle = 1
+	}
+
+	if str := q.Get("maxOpen"); str != "" {
+		q.Del("maxOpen")
+		ds.maxOpen = parseInt(str)
+	} else {
+		ds.maxOpen = 10
+	}
+
+	if str := q.Get("maxLife"); str != "" {
+		q.Del("maxLife")
+		ds.maxLife = parseDuration(str)
+	} else {
+		ds.maxLife = 10 * time.Minute
+	}
+
+	return ds
+}
+
+func parseInt(str string) int {
+	value, err := strconv.Atoi(str)
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
+}
+
+func parseDuration(str string) time.Duration {
+	value, err := time.ParseDuration(str)
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
 }
--- config/db/postgres/postgres.go
+++ config/db/postgres/postgres.go
@@ -3,6 +3,8 @@ package postgres
 import (
 	"database/sql"
 	"log"
+	"net/url"
+	"strconv"
 	"sync"
 	"time"
 
@@ -28,15 +30,70 @@ func DB(name string) *bsql.DB {
 }
 
 func newDB(name string) *bsql.DB {
-	db, err := sql.Open(`postgres`, config.Get(`postgres`).GetString(name))
+	dataSource := newDataSource(config.Get("postgres").GetString(name))
+	db, err := sql.Open("postgres", dataSource.url.String())
 	if err != nil {
 		log.Panic(err)
 	}
 	if err := db.Ping(); err != nil {
 		log.Panic(err)
 	}
-	db.SetConnMaxLifetime(time.Minute * 10)
-	db.SetMaxIdleConns(5)
-	db.SetMaxOpenConns(50)
+	db.SetMaxOpenConns(dataSource.maxOpen)
+	db.SetMaxIdleConns(dataSource.maxIdle)
+	db.SetConnMaxLifetime(dataSource.maxLife)
 	return bsql.New(db, 5*time.Second)
+}
+
+type dataSource struct {
+	url              *url.URL
+	maxIdle, maxOpen int
+	maxLife          time.Duration
+}
+
+func newDataSource(urlStr string) dataSource {
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		log.Panic(err)
+	}
+	ds := dataSource{url: u}
+
+	q := u.Query()
+	if str := q.Get("maxIdle"); str != "" {
+		q.Del("maxIdle")
+		ds.maxIdle = parseInt(str)
+	} else {
+		ds.maxIdle = 1
+	}
+
+	if str := q.Get("maxOpen"); str != "" {
+		q.Del("maxOpen")
+		ds.maxOpen = parseInt(str)
+	} else {
+		ds.maxOpen = 10
+	}
+
+	if str := q.Get("maxLife"); str != "" {
+		q.Del("maxLife")
+		ds.maxLife = parseDuration(str)
+	} else {
+		ds.maxLife = 10 * time.Minute
+	}
+
+	return ds
+}
+
+func parseInt(str string) int {
+	value, err := strconv.Atoi(str)
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
+}
+
+func parseDuration(str string) time.Duration {
+	value, err := time.ParseDuration(str)
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
 }
