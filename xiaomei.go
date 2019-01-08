fb4baf7b89c03a138a88ac93e2e64a6fba09c531
bughou <applejava@qq.com>
2019-01-08 21:34:43 +0800

    update version

--- xiaomei.go
+++ xiaomei.go
@@ -39,7 +39,7 @@ func versionCmd() *cobra.Command {
 		Use:   `version`,
 		Short: `show xiaomei version.`,
 		RunE: release.NoArgCall(func() error {
-			fmt.Println(`xiaomei version 18.12.22`)
+			fmt.Println(`xiaomei version 19.01.08`)
 			return nil
 		}),
 	}
