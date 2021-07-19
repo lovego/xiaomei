fea19194a9dc87dabcbf06c3fb0ab5e6486233b5
bughou <bughou@gmail.com>
2021-07-19 16:09:55 +0800

    detectDir optimize

--- release/root.go
+++ release/root.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/lovego/cmd"
-	"github.com/lovego/fs"
 )
 
 var theRoot *string
@@ -27,11 +26,13 @@ func InProject() bool {
 func detectRoot() string {
 	if theRoot == nil {
 		if cwd, err := os.Getwd(); err != nil {
-			panic(err)
-		} else if dir := fs.DetectDir(cwd, `release/img-app/config/config.yml`); dir != `` {
-			dir = filepath.Join(dir, `release`)
-			theRoot = &dir
-		} else if dir := fs.DetectDir(cwd, `release/config.yml`); dir != `` {
+			log.Panic(err)
+		} else if dir := detectDir(cwd,
+			`release/img-app/config/config.yml`,
+			`release/img-app/config_*/config.yml`,
+			`release/config.yml`,
+			`release/config_*.yml`,
+		); dir != `` {
 			dir = filepath.Join(dir, `release`)
 			theRoot = &dir
 		} else {
@@ -50,4 +51,24 @@ func ModulePath() (string, error) {
 		return ``, err
 	}
 	return strings.TrimSpace(output), nil
+}
+
+func detectDir(dir string, features ...string) string {
+	for ; dir != `/`; dir = filepath.Dir(dir) {
+		if hasAnyFeatures(dir, features) {
+			return dir
+		}
+	}
+	return ``
+}
+
+func hasAnyFeatures(dir string, features []string) bool {
+	for _, feature := range features {
+		if out, err := filepath.Glob(filepath.Join(dir, feature)); err != nil {
+			log.Panic(err)
+		} else if len(out) > 0 {
+			return true
+		}
+	}
+	return false
 }
